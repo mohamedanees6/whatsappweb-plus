39aa31ae6dc8391ae114b0d74febb5e627394aaf
Mohamed Anees <anees.m@practo.com>
2019-02-19 22:40:53 +0530

    Init commit

--- src/com/anees/develop/whatsapp/WhatsappScrape.java
+++ src/com/anees/develop/whatsapp/WhatsappScrape.java
@@ -1,7 +1,13 @@
 package com.anees.develop.whatsapp;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WhatsappScrape {
 	public static void main(String args[]) throws Exception {
-
+		ChromeDriver cd = new ChromeDriver();
+		cd.get("web.whatsapp.com");
+		
+       
 	}
 
 }
